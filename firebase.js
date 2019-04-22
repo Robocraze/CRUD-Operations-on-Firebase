299f3641ba7a62f36953d2be280d48ce0b533b2e
Robocraze <48519472+Robocraze@users.noreply.github.com>
2019-04-22 18:45:05 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,116 +1,68 @@
 
 const firebase=require('firebase')
-//Config file
 
 
-
+//COPY PASTE CONGIF OBJECT FROM FIREBASE CONSOLE
 var config = {
-    apiKey: "AIzaSyC75pTZQplqXcycft-4hLiiaE8ck_Jk3lU",
-    authDomain: "robocraze-ac834.firebaseapp.com",
-    databaseURL: "https://robocraze-ac834.firebaseio.com",
-    projectId: "robocraze-ac834",
-    storageBucket: "robocraze-ac834.appspot.com",
-    messagingSenderId: "580150101989"
+    apiKey: "API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    databaseURL: "DATABASE_URL",
+    projectId: "PROJECT_ID",
+    storageBucket: "STORAGE_BUCKET"
+    messagingSenderId: "MESSAGING_SENDER_ID"
   };
   firebase.initializeApp(config);
 
-  
-  
-  const database=firebase.database();
-//   const usersRef=database.ref('/users');
+//CREATE DATABASE REFERENCE VARIABLE
+const database=firebase.database();
 
+//POINT TO USERS NODE
+const usersRef=database.ref('/users');
 
-  //set-Writes or replace data to a defined path
-
-  database.ref('users/').set({
-      user1:{
-    username:'Mel',
-    email:'melgo@gmail.com',
-  }},
-    function(error) {
-        if (error) {
-          console.log("Data could not be saved." + error);
-        } else {
-          console.log("Data saved successfully.");
-        }
-    
+// set-Writes or replace data to a defined path
+database.ref('users/').set({
+  user1:{
+username:'Mel',
+email:'melgo@gmail.com',
+}},
+function(error) {
+    if (error) {
+      console.log("Data could not be saved." + error);
+    } else {
+      console.log("Data saved successfully.");
+    }
 })
 
 
+//UPDATE DATA TO A SPECIFIED PATH
+const user1Ref = usersRef.child("user1");
+user1Ref.update({
+  "username": "dylan"
+},);
 
+// push() function  generates a unique key for each new child. 
+usersRef.push().set({
 
-// const user1Ref = usersRef.child("user1");
-// user1Ref.update({
-//   "username": "dylan"
-// },);
+    username:'Daniel',
+    email:'daniel@gmail.com'
+})
 
-
-
-
-
-// // push() function  generates a unique key for each new child. 
-
-// usersRef.push().set({
-
-//     username:'Daniel',
-//     email:'daniel@gmail.com'
-
-// })
-
-
-
-
-
-
-
-
-
-
-// usersRef.on("value", function(snapshot) {
-//     console.log(snapshot.val());
-//   }, function (errorObject) {
-//     console.log("The read failed: " + errorObject.code);
-//   });
+//READ DATA FROM A SPECIFIC PATH
+usersRef.on("value", function(snapshot) {
+    console.log(snapshot.val());
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
   
-//   usersRef.once("value", function(snapshot) {
-//     console.log(snapshot.val());
-//   }, function (errorObject) {
-//     console.log("The read failed: " + errorObject.code);
-//   })
+  usersRef.once("value", function(snapshot) {
+    console.log(snapshot.val());
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  })
   
-  
-
-
-
-
-
-
-
-
-const usersRef=database.ref('/users');
+//REMOVE A RECORD
 
 usersRef.child("user1").remove();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // //add timestamp
-
-
-//   usersRef.push({ startedAt: firebase.database.ServerValue.TIMESTAMP });
+// ADD CURRENT TIMESTAMP TO RECORD
+usersRef.push({ startedAt: firebase.database.ServerValue.TIMESTAMP });
